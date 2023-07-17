33d2551262b52d1c90e32df44eeccc478a1cdf5a
sagarr-singh <sagarsatendrasingh408@gmail.com>
2023-07-16 22:41:41 -0700

    updated

--- src/screen/Contact.js
+++ src/screen/Contact.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import '../App.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <div>
+        <form className='formm'>
+          <span className='headForm'>Get in touch with <b>SAKSHI SINGH</b></span><br/><br/>
+          <input type='text' placeholder='Your Name'/><br/>
+          <input type='text' placeholder='Your Designation'/><br/>
+          <input type='text' placeholder='Your Contact'/><br/>
+          <textarea placeholder='Your Message' rows={4} cols={50}/>
+        </form>
+      </div>
+    </>
   )
 }
 
